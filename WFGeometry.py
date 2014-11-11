edd0e988a10d5600ce587753f5fe1d58b97c689f
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-11-11 19:34:44 +0100

    1) Added EventDispatcher class in WFGeometry.py

--- WFGeometry.py
+++ WFGeometry.py
@@ -14,6 +14,9 @@
 
 
 
+from collections import defaultdict
+
+
 
 def main():
 
@@ -23,6 +26,90 @@ def main():
 	'''
 
 	pass
+
+
+
+class EventDispatcher:
+
+	'''
+	Introduces dynamic event binding to Pygame.
+
+	Add, replace and query event handlers
+
+	'''
+
+	# TODO: Normalize event data (?)
+	# TODO: Event aliases, tkinter-style event definitions (?)
+	# eg. type=KEYDOWN key=LEFT
+	# TODO: Hierachies of events, multiple handlers, replace or add
+	# TODO: Pattern or Event class implementing aliases, comparisons, etc.
+	# TODO: Queries
+	# TODO: Nested dicts or objects (?)
+	# TODO: Handle polling as well (?)
+	# TODO: Annotations, error handling
+
+	def __init__(self):
+		
+		'''
+		Docstring goes here
+
+		'''
+
+		# TODO: Is this the optimal data structure (w.r.t RAM and CPU)
+		# TODO: Allow multiple handlers per pattern
+		self.handlers = defaultdict(defaultdict(list)) # Maps event types to patterns and their respective handlers
+
+
+	def handle(self, event):
+		
+		'''
+		Dispatches an event to all matching event handlers.
+
+		'''
+
+		for handler in self.query(event): #getattr(self, event.type)
+			handler(event)
+			print('Invalid handler type')
+
+
+	def query(self, event):
+
+		'''
+		Retrieves all event handlers whose patterns
+		matches that of the incoming event (cf. bind for details).
+
+		'''
+
+		# TODO: Use issubset for comparisons
+		# TODO: Determine and - if necessary - improve performance
+		match = frozenset({attr: getattr(event, attr) for attr in ['type', 'key', 'button', 'rel'] if hasattr(event, attr) })
+
+		# TODO: Optionally return mapping between matching patterns and their corresponding handlers (?)
+		return (handler(event) for handler in handlers for pattern, handlers in self.handlers[event.type].items() if pattern.issubset(match))
+
+
+	def bind(self, pattern, handler, replace=False):
+
+		'''
+		Binds a handler to an event pattern, optionally
+		replacing any pre-existing handlers.
+
+		A pattern is defined as a set of properties which an event must have
+		to be considered a match (eg. event.type is KEYDOWN and event.key is K_LEFT)
+		
+
+		Returns an ID associated with the particular handler.
+
+		'''
+
+		# TODO: Implement replace option
+		# TODO: Use issubset for comparisons
+		# TDOO: Should the type property be mandatory (?)
+
+		key = frozenset(*pattern.items)
+		self.handlers[pattern.type][key].append(handler)
+		return hash(key), id(handler)
+
 
 
 # Basic OBJ file viewer. needs objloader from:
