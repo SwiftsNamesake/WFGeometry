f25b60efdc451850863e0c4b8f0b861f0352dd5d
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-11-11 18:20:37 +0100

    1) Changed selected model 2) Added left/right arrow behaviour

--- WFGeometry.py
+++ WFGeometry.py
@@ -56,7 +56,7 @@ glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
  
 # LOAD OBJECT AFTER PYGAME INIT
 # obj = OBJ(sys.argv[1], swapyz=True)
-obj = OBJ(['data/villa.obj', 'data/square.obj'][1], swapyz=False)
+obj = OBJ(['data/villa.obj', 'data/square.obj'][0], swapyz=False)
 
  
 clock = pygame.time.Clock()
@@ -74,39 +74,51 @@ zpos = 5
 rotate = move = False
 
 while 1:
-    clock.tick(30)
-    for e in pygame.event.get():
-        if e.type == QUIT:
-            sys.exit()
-        elif e.type == KEYDOWN and e.key == K_ESCAPE:
-            sys.exit()
-        elif e.type == MOUSEBUTTONDOWN:
-            if e.button == 4: zpos = max(1, zpos-1)
-            elif e.button == 5: zpos += 1
-            elif e.button == 1: rotate = True
-            elif e.button == 3: move = True
-        elif e.type == MOUSEBUTTONUP:
-            if e.button == 1: rotate = False
-            elif e.button == 3: move = False
-        elif e.type == MOUSEMOTION:
-            i, j = e.rel
-            if rotate:
-                rx += i
-                ry += j
-            if move:
-                tx += i
-                ty -= j
+	clock.tick(30)
+	for e in pygame.event.get():
+		if e.type == QUIT:
+			sys.exit()
+		elif e.type == KEYDOWN:
+			if e.key == K_ESCAPE:
+				sys.exit()
+			elif e.key in (K_LEFT, K_RIGHT):
+				print('Key rotation')
+				rotate = True
+				i, j = (2 if e.key == K_LEFT else -2, 0)
+		elif e.type == KEYUP:
+			if e.key in (K_LEFT, K_RIGHT):
+				rotate = False
+		elif e.type == MOUSEBUTTONDOWN:
+			if e.button == 4: zpos = max(1, zpos-1)
+			elif e.button == 5: zpos += 1
+			elif e.button == 1: rotate = True
+			elif e.button == 3: move = True
+		elif e.type == MOUSEBUTTONUP:
+			if e.button == 1: rotate = False
+			elif e.button == 3: move = False
+		elif e.type == MOUSEMOTION:
+			i, j = e.rel
+			if rotate:
+				rx += i
+				ry += j
+			if move:
+				tx += i
+				ty -= j
+
+	if rotate:
+		rx += i
+		ry += j
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glLoadIdentity()
  
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
+	# RENDER OBJECT
+	glTranslate(tx/20., ty/20., - zpos)
+	glRotate(ry, 1, 0, 0)
+	glRotate(rx, 0, 1, 0)
+	glCallList(obj.gl_list)
  
-    # RENDER OBJECT
-    glTranslate(tx/20., ty/20., - zpos)
-    glRotate(ry, 1, 0, 0)
-    glRotate(rx, 0, 1, 0)
-    glCallList(obj.gl_list)
- 
-    pygame.display.flip()
+	pygame.display.flip()
 
 if __name__ == '__main__':
 	main()
